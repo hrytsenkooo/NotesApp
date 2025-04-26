8003d515fa135921f0785875cbf9bba852c731e4
Hrytsenko Artem <hrytsenko.artem@lll.kpi.ua>
2025-04-26 18:02:20 +0300

    feat: implemented lambda functions

---
+++ NotesApp.Lambda/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NotesApp.Application;
+using NotesApp.Infrastructure;
+
+namespace NotesApp.Lambda
+{
+    public class Startup
+    {
+        public static ServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            services.AddApplication();
+            services.AddInfrastructure();
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
