d8fbfcc20eda274d3c4a90d53c3d73142012a206
Hrytsenko Artem <hrytsenko.artem@lll.kpi.ua>
2025-04-26 15:54:36 +0300

    feat: registered services

---
+++ NotesApp.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Amazon.DynamoDBv2;
+using Microsoft.Extensions.DependencyInjection;
+using NotesApp.Domain.Interfaces;
+using NotesApp.Infrastructure.Repositories;
+
+namespace NotesApp.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddSingleton<IAmazonDynamoDB>(sp =>
+            {
+                var clientConfig = new AmazonDynamoDBConfig();
+                return new AmazonDynamoDBClient(clientConfig);
+            });
+
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<INoteRepository, NoteRepository>();
+
+            return services;
+        }
+    }
+}
