b623396be9d351f28639c8f9b0185b667c28416a
Hrytsenko Artem <hrytsenko.artem@lll.kpi.ua>
2025-04-26 15:51:06 +0300

    feat: implemented repositories

---
+++ NotesApp.Infrastructure/Repositories/NoteRepository.cs
@@ -0,0 +1,81 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using NotesApp.Domain.Interfaces;
+using NotesApp.Domain.Models;
+using NotesApp.Infrastructure.Data.Mappers;
+using NotesApp.Infrastructure.Data.Models;
+
+namespace NotesApp.Infrastructure.Repositories
+{
+    public class NoteRepository : INoteRepository
+    {
+        private readonly IDynamoDBContext _context;
+
+        public NoteRepository(IAmazonDynamoDB dynamoDbClient)
+        {
+            _context = new DynamoDBContext(dynamoDbClient);
+        }
+
+        public async Task<Note> CreateAsync(Note note)
+        {
+            note.Id = Guid.NewGuid().ToString();
+            note.CreatedAt = DateTime.UtcNow;
+            note.UpdatedAt = DateTime.UtcNow;
+
+            var item = note.ToNoteItem();
+            await _context.SaveAsync(item);
+
+            return note;
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+            await _context.DeleteAsync<NoteItem>(id);
+        }
+
+        public async Task<IEnumerable<Note>> GetAllAsync()
+        {
+            var scan = _context.ScanAsync<NoteItem>(new List<ScanCondition>());
+            var items = await scan.GetRemainingAsync();
+
+            var notes = new List<Note>();
+            foreach (var item in items)
+            {
+                notes.Add(item.ToNote());
+            }
+
+            return notes;
+        }
+
+        public async Task<Note> GetByIdAsync(string id)
+        {
+            var item = await _context.LoadAsync<NoteItem>(id);
+            return item?.ToNote();
+        }
+
+        public async Task<IEnumerable<Note>> GetByUserIdAsync(string userId)
+        {
+            var query = _context.QueryAsync<NoteItem>(userId, new DynamoDBOperationConfig { IndexName = "UserIdIndex" });
+
+            var items = await query.GetRemainingAsync();
+
+            var notes = new List<Note>();
+            foreach (var item in items)
+            {
+                notes.Add(item.ToNote());
+            }
+
+            return notes;
+        }
+
+        public async Task<Note> UpdateAsync(Note note)
+        {
+            note.UpdatedAt = DateTime.UtcNow;
+
+            var item = note.ToNoteItem();
+            await _context.SaveAsync(item);
+
+            return note;
+        }
+    }
+}
---
+++ NotesApp.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,86 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using NotesApp.Domain.Interfaces;
+using NotesApp.Domain.Models;
+using NotesApp.Infrastructure.Data.Mappers;
+using NotesApp.Infrastructure.Data.Models;
+
+namespace NotesApp.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly IDynamoDBContext _context;
+
+        public UserRepository(IAmazonDynamoDB dynamoDbClient)
+        {
+            _context = new DynamoDBContext(dynamoDbClient);
+        }
+
+        public async Task<User> CreateAsync(User user)
+        {
+            user.Id = Guid.NewGuid().ToString();
+            user.CreatedAt = DateTime.UtcNow;
+            user.UpdatedAt = DateTime.UtcNow;
+
+            var item = user.ToUserItem();
+            await _context.SaveAsync(item);
+
+            return user;
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+            await _context.DeleteAsync<UserItem>(id);
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            var scan = _context.ScanAsync<UserItem>(new List<ScanCondition>());
+            var items = await scan.GetRemainingAsync();
+
+            var users = new List<User>();
+            foreach (var item in items)
+            {
+                users.Add(item.ToUser());
+            }
+
+            return users;
+        }
+
+        public async Task<User> GetByEmailASync(string email)
+        {
+            var query = _context.QueryAsync<UserItem>(email, new DynamoDBOperationConfig { IndexName = "EmailIndex" });
+
+            var items = await query.GetRemainingAsync();
+            var item = items.Count > 0 ? items[0] : null;
+
+            return item?.ToUser();
+        }
+
+        public async Task<User> GetByIdAsync(string id)
+        {
+            var item = await _context.LoadAsync<UserItem>(id);
+            return item?.ToUser();
+        }
+
+        public async Task<User> GetByUsernameAsync(string username)
+        {
+            var query = _context.QueryAsync<UserItem>(username, new DynamoDBOperationConfig { IndexName = "UsernameIndex" });
+
+            var items = await query.GetRemainingAsync();
+            var item = items.Count > 0 ? items[0] : null;
+
+            return item?.ToUser();
+        }
+
+        public async Task<User> UpdateAsync(User user)
+        {
+            user.UpdatedAt = DateTime.UtcNow;
+
+            var item = user.ToUserItem();
+            await _context.SaveAsync(item);
+
+            return user;
+        }
+    }
+}
